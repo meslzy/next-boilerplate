24dde588bdb82137b519c1b2a7f5a271330657b4
Meslzy <imeslzy@gmail.com>
2023-06-14 18:59:09 +0000

    Update 3 files

--- src/app/[lang]/ui.tsx
+++ src/app/[lang]/ui.tsx
@@ -1,14 +1,62 @@
 "use client";
 
-import { Text } from "@mantine/core";
+import useSWR from "swr";
+import axios from "axios";
 
-import { useTranslation } from "~i18n/client";
+const useGithubRepo = (name: string) => {
+  const { data, error, isLoading } = useSWR(name, (url) => {
+    return axios
+      .get(url, {
+        baseURL: "https://api.github.com/repos/rosestack",
+      })
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        throw error;
+      });
+  });
+
+  return {
+    data,
+    error,
+    isLoading,
+  };
+};
+
+const useNpmDownloads = (packageName: string) => {
+  if ( packageName.startsWith("https://www.npmjs.com/package/") ) {
+    packageName = packageName.replace("https://www.npmjs.com/package/", "");
+  }
+  
+  const { data, error, isLoading } = useSWR<string>(packageName, (url) => {
+    return axios.get(url, {
+      baseURL: "https://api.npmjs.org/downloads/point/last-year",
+    }).then((response) => {
+      return response.data.downloads;
+    }).catch((error) => {
+      throw error;
+    });
+  });
+
+  return {
+    data,
+    error,
+    isLoading,
+  };
+};
 
 const Ui = () => {
-  const t = useTranslation();
+  const githubRepo = useGithubRepo("rosedb");
+  const npmDownloads = useNpmDownloads("https://www.npmjs.com/package/rosedb");
 
+  console.log(githubRepo);
+  console.log(npmDownloads);
+  
   return (
-    <Text>{ t("hello") }</Text>
+    <div>
+      <p>done</p>
+    </div>
   );
 };
 
